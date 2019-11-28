f14d60fe4ebcfb894a8a8b479c2b264a8adb5f44
huangxok <huangx.ok@163.com>
2019-11-28 06:25:59 +0000

    Deploying to gh-pages from master 6055981e2269eeee82e1155341aa26d214abdb5f

---
+++ precache-manifest.27ea34c9694df5dd17f0035def7edef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8ba51310575ad4e4d082f7c2f0f66e",
+    "url": "/github-action-demo/index.html"
+  },
+  {
+    "revision": "dd8d25a778776bea463a",
+    "url": "/github-action-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "555149349d442c73b1c4",
+    "url": "/github-action-demo/static/js/2.dbe21ac2.chunk.js"
+  },
+  {
+    "revision": "dd8d25a778776bea463a",
+    "url": "/github-action-demo/static/js/main.aedb39d8.chunk.js"
+  },
+  {
+    "revision": "2aea3602368b840de1ba",
+    "url": "/github-action-demo/static/js/runtime-main.e78d1611.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github-action-demo/static/media/logo.25bf045c.svg"
+  }
+]);
